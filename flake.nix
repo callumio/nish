cae2526833e45739ba4d229e028c18d953ebc795
Callum Leslie <git@cleslie.uk>
2024-09-11 08:56:22 +0100

    chore: use fenix and flake-parts

--- flake.nix
+++ flake.nix
@@ -1,11 +1,12 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
+    fenix.url = "github:nix-community/fenix/pull/163/head";
 
     crane.url = "github:ipetkov/crane";
 
@@ -13,26 +14,33 @@
       url = "github:rustsec/advisory-db";
       flake = false;
     };
+
+    systems.url = "github:nix-systems/default";
+
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
-    rust-overlay,
-    crane,
-    advisory-db,
-  }:
-    flake-utils.lib.eachDefaultSystem
-    (
-      system: let
-        overlays = [(import rust-overlay)];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
+    flake-parts,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit self inputs;} {
+      systems = import inputs.systems;
 
-        rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
-        src = craneLib.cleanCargoSource ./.;
+      perSystem = {
+        pkgs,
+        inputs',
+        self',
+        ...
+      }: let
+        rustToolchain = inputs'.fenix.packages.fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          sha256 = "sha256-VZZnlyP69+Y3crrLHQyJirqlHrTtGTsyiSnZB8jEvVo=";
+        };
+        craneLib = (inputs.crane.mkLib pkgs).overrideToolchain rustToolchain;
+        src = craneLib.cleanCargoSource (craneLib.path ./.);
 
         nativeBuildInputs = with pkgs; [rustToolchain pkg-config];
         buildInputs = with pkgs; [udev];
@@ -43,39 +51,36 @@
 
         cargoArtifacts = craneLib.buildDepsOnly commonArgs;
         bin = craneLib.buildPackage (commonArgs // {inherit cargoArtifacts;});
-      in
-        with pkgs; {
-          checks = {
-            inherit bin;
-            clippy = craneLib.cargoClippy (commonArgs
-              // {
-                inherit cargoArtifacts;
-                cargoClippyExtraArgs = "--all-targets -- --deny warnings";
-              });
-            coverage = craneLib.cargoTarpaulin (commonArgs // {inherit cargoArtifacts;});
-            fmt = craneLib.cargoFmt {inherit src;};
-            audit = craneLib.cargoAudit {
-              inherit src advisory-db;
-            };
-          };
+      in {
+        _module.args.pkgs = inputs'.nixpkgs.legacyPackages.extend inputs.fenix.overlays.default;
 
-          packages = {
-            default = bin;
+        checks = {
+          inherit bin;
+          clippy = craneLib.cargoClippy (commonArgs
+            // {
+              inherit cargoArtifacts;
+              cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+            });
+          coverage = craneLib.cargoTarpaulin (commonArgs // {inherit cargoArtifacts;});
+          fmt = craneLib.cargoFmt {inherit src;};
+          audit = craneLib.cargoAudit {
+            inherit src;
+            inherit (inputs) advisory-db;
           };
+        };
+        packages = {
+          default = bin;
+        };
 
-          apps = rec {
-            default = flake-utils.lib.mkApp {drv = bin;};
-          };
-
-          devShells.default = craneLib.devShell {
-            checks = self.checks.${system};
-            packages = [];
-          };
-        }
-    );
+        devShells.default = craneLib.devShell {
+          inherit (self') checks;
+          packages = [];
+        };
+      };
+    };
 
   nixConfig = {
-    extra-substituters = ["callumio-public.cachix.org"];
+    extra-substituters = ["https://callumio-public.cachix.org"];
     extra-trusted-public-keys = ["callumio-public.cachix.org-1:VucOSl7vh44GdqcILwMIeHlI0ufuAnHAl8cO1U/7yhg="];
   };
 }
